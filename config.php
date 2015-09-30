d6343dd605337a5094683da0eff9aeb16f460541
Mauricio Guariero Dias <mguariero@gmail.com>
2015-09-29 22:32:13 -0400

    Adicionado comentarios

--- config.php
+++ config.php
@@ -1,15 +1,20 @@
 <?php
+
 date_default_timezone_set("America/Campo_Grande");
+
 ##################################
 # CONFIGURAÇÕES DO SGBD 
 ##################################
 
 # servidor do SGBD:
 Config::set('db_host', 'localhost');
+
 # usuario do SGBD:
 Config::set('db_user', 'root');
+
 # senha do SGBD:
 Config::set('db_password', '');
+
 # nome do banco de dados:
 Config::set('db_name', '');
 
@@ -19,7 +24,6 @@ Config::set('db_name', '');
 Config::set('debug', true);
 
 # TEMPLATE PADRÃO
-# Ex: para o diretório /template/default:
 # Config::set('template', 'default');
 Config::set('template', 'default');
 
@@ -32,8 +36,18 @@ Config::set('key', 'bF3mMstKyG96AhBfGjfubQRR7W5Mrpnwarzq7GWufVyfZwrzttmAnP75wqvH
 # Utiliza uma chave alfa-numéria complexa de no mínimo 16 dígitos
 Config::set('salt', 'TjcqT8jgR8H6v6vhJhBcdZmCvnZs4Ghs9JcvW48gCqUTtVEkcK5hYLpsw6As8AbU');
 
+# Define a linguagem do Sistema
 Config::set('lang', 'pt_br');
+
+# Regra de Reescrita (URLs Amigáveis)
 Config::set('rewriteURL', true);
+
+# Controller principal
 Config::set('indexController', 'Index');
+
+# Action principal
 Config::set('indexAction', 'index');
+
+# Parametros que devem ser Criptografados 
+# ex: Config::set('criptedGetParamns', array('id','codigo','telefone'))
 Config::set('criptedGetParamns', array());
